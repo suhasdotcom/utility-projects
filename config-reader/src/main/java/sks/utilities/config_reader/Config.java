83394914ec14704a29a033e02dc3b254cbc7b200
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-08-17 16:02:37 +0530

    config-reader: added config examples to config-reader.

---
+++ config-reader/src/main/java/sks/utilities/config_reader/Config.java
@@ -0,0 +1,10 @@
+package sks.utilities.config_reader;
+
+public class Config {
+    public static int retry;
+    public static String name;
+
+    public static class Inner {
+        public static String name;
+    }
+}
