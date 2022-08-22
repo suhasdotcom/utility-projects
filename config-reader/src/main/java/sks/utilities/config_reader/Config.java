a0c3520e747b029e277061a2eef26b4034a407ca
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-08-22 22:04:04 +0530

    config-reader: added ConfigFilePath annotation.

--- config-reader/src/main/java/sks/utilities/config_reader/Config.java
+++ config-reader/src/main/java/sks/utilities/config_reader/Config.java
@@ -1,5 +1,8 @@
 package sks.utilities.config_reader;
 
+import sks.utilities.config_reader.annotations.ConfigFilePath;
+
+@ConfigFilePath("app-config.cfg")
 public class Config {
     public static int retry;
     public static String name;
---
+++ config-reader/src/main/java/sks/utilities/config_reader/annotations/ConfigFilePath.java
@@ -0,0 +1,21 @@
+package sks.utilities.config_reader.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation is to be used on the class which will behave as the config file's value taker in java.
+ * The {@link ConfigFilePath#filePath filePath} is the config file's path relative to src/main/resources or src/test/resources
+ * in case this is not the file hierarchy, then config file's path from project's root can be given as "/project-dir/sub-dir/../sub-dir/config-file-name.{cfg/properties/json/yaml} etc
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ConfigFilePath {
+    /**
+     * The config file's path relative to src/main/resources or src/test/resources
+     * in case this is not the file hierarchy, then config file's path from project's root can be given as "/project-dir/sub-dir/../sub-dir/config-file-name.{cfg/properties/json/yaml} etc
+     */
+    public String value();
+}
