965767d9401d98b035be09474f1d0097a6f298fb
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-08-25 00:44:57 +0530

    config-reader: no longer need Main Driver. Things will run via tests now.

--- config-reader/src/main/java/sks/utilities/config_reader/Main.java
+++
@@ -1,8 +0,0 @@
-package sks.utilities.config_reader;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        ConfigReader.startReading(Config.class);
-    }
-}
