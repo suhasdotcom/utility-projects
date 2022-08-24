be4af1c18a805d95792ef0215fa21c83a666ca61
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-08-25 00:26:39 +0530

    config-reader: corrected javadoc for annotation ConfigFilePath.

--- config-reader/src/main/java/sks/utilities/config_reader/annotations/ConfigFilePath.java
+++ config-reader/src/main/java/sks/utilities/config_reader/annotations/ConfigFilePath.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 /**
  * This annotation is to be used on the class which will behave as the config file's value taker in java.
- * The {@link ConfigFilePath#filePath filePath} is the config file's path relative to src/main/resources or src/test/resources
+ * The {@link ConfigFilePath#value filePath} is the config file's path relative to src/main/resources or src/test/resources
  * in case this is not the file hierarchy, then config file's path from project's root can be given as "/project-dir/sub-dir/../sub-dir/config-file-name.{cfg/properties/json/yaml} etc
  */
 @Target(ElementType.TYPE)
