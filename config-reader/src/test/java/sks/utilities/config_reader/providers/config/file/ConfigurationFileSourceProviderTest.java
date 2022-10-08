fc502747df508aec25b0cec2016704dd6d0a9874
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-10-09 01:56:04 +0530

    config-reader: Added more test cases to ConfigurationFileSourceProviderTest.MAIN_CLASSPATH_LIKE_Test.

--- config-reader/src/test/java/sks/utilities/config_reader/providers/config/file/ConfigurationFileSourceProviderTest.java
+++ config-reader/src/test/java/sks/utilities/config_reader/providers/config/file/ConfigurationFileSourceProviderTest.java
@@ -18,10 +18,21 @@ public class ConfigurationFileSourceProviderTest
     {
         @ParameterizedTest
         @DisplayName("only look for files with a simple pattern - positive test")
-        @ValueSource(strings = {"app-config.cfg", "appConfig.properties", "someDir/app-conf.conf", "someDir/Configuration_New.json"})
+        @ValueSource(strings = {"app-config.cfg", "appConfig.properties", "someDir/app-conf.conf", "someDir/Configuration_New.json",
+                                "rootDir/someDir/_filename-name-1.yaml"})
         public void testSimpleFileNamePattern(String fileName)
         {
             assertTrue(ConfigurationFileSourceProvider.MAIN_CLASSPATH_LIKE.getValidationFunc().test(fileName));
+        }
+
+
+        @ParameterizedTest
+        @DisplayName("only look for files with a simple pattern - negative test")
+        @ValueSource(strings = {":app-config.cfg", "/appConfig.properties", "/someDir/app-conf.conf", ":someDir/Configuration_New.json",
+                "~/rootDir/someDir/_filename-name-1.yaml"})
+        public void testSimpleFileNamePatternNegative(String fileName)
+        {
+            assertFalse(ConfigurationFileSourceProvider.MAIN_CLASSPATH_LIKE.getValidationFunc().test(fileName));
         }
     }
 }
