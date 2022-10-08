66533ff10d96f83c45697d289303ad9adcb11a92
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-10-09 01:37:03 +0530

    config-reader: Added documentation to ConfigurationSourceProvider.

--- config-reader/src/main/java/sks/utilities/config_reader/providers/config/ConfigurationSourceProvider.java
+++ config-reader/src/main/java/sks/utilities/config_reader/providers/config/ConfigurationSourceProvider.java
@@ -1,5 +1,8 @@
 package sks.utilities.config_reader.providers.config;
 
+/**
+ * Provide {@link sks.utilities.config_reader.model.ConfigurationSource ConfiguratonSource} for different kind of configurations
+ */
 public interface ConfigurationSourceProvider
 {
     String getConfigurationSource();
