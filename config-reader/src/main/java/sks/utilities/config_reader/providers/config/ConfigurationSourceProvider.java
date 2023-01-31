fb68ef475d7cd1745cf63abdd39c84197856f402
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2023-01-31 20:48:33 +0530

    config-reader: Added required javadoc

--- config-reader/src/main/java/sks/utilities/config_reader/providers/config/ConfigurationSourceProvider.java
+++ config-reader/src/main/java/sks/utilities/config_reader/providers/config/ConfigurationSourceProvider.java
@@ -5,5 +5,9 @@ package sks.utilities.config_reader.providers.config;
  */
 public interface ConfigurationSourceProvider
 {
+    /**
+     *
+     * @return Name of the expected configuration source
+     */
     String getConfigurationSource();
 }
