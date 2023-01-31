b53f25244a7dd41ade6bed1765e1d2a201ffda83
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2023-02-01 00:09:33 +0530

    config-reader: Added tests to get conformingFileSource in the class.

--- config-reader/src/test/java/sks/utilities/config_reader/ConfigReaderTest.java
+++ config-reader/src/test/java/sks/utilities/config_reader/ConfigReaderTest.java
@@ -4,12 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-
 import sks.utilities.config_reader.annotations.ConfigFilePath;
 import sks.utilities.config_reader.config.ConfigClass;
 import sks.utilities.config_reader.providers.config.file.ConfigurationFileSourceProvider;
 
-import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -23,15 +21,17 @@ class ConfigReaderTest
     class GetCorrectConfigFile
     {
 
+        private String filePath;
+
         @BeforeEach
         public void setUp() {
             ConfigReader.startReading(ConfigClass.class);
+            filePath = ConfigClass.class.getDeclaredAnnotation(ConfigFilePath.class).value();
         }
 
         @Test
         @DisplayName("path taken up for config file should be correct")
-        void testPathPickedUpForConfigFile() {
-            String filePath = ConfigClass.class.getDeclaredAnnotation(ConfigFilePath.class).value();
+        public void testPathPickedUpForConfigFile() {
             assertEquals(filePath, ":test/resources/app-config.cfg");
             assertTrue(ConfigurationFileSourceProvider.PROJECT_SOURCE.getValidationFunc().test(filePath));
             assertEquals(ConfigurationFileSourceProvider.PROJECT_SOURCE.getConfiguredPath(filePath),
@@ -40,9 +40,14 @@ class ConfigReaderTest
 
         @Test
         @DisplayName("file is read as wanted")
-        void testFileReadCorrectly() {
-            final String filePath = ConfigClass.class.getDeclaredAnnotation(ConfigFilePath.class).value();
+        public void testFileReadCorrectly() {
 
+        }
+
+        @Test
+        @DisplayName("Get the correct file configuration type")
+        public void getCorrectFileConfigurationType() {
+            assertEquals(ConfigurationFileSourceProvider.PROJECT_SOURCE, ConfigurationFileSourceProvider.getConformingFileSource(filePath));
         }
     }
 }
