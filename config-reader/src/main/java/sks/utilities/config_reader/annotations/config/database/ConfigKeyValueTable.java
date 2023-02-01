2bb1dcb34ff4f9d6c6232173e659f0acb1a08564
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2023-02-01 17:32:56 +0530

    config-reader: Added ConfigKeyValueTable annotation to support reading configurations from a database.

---
+++ config-reader/src/main/java/sks/utilities/config_reader/annotations/config/database/ConfigKeyValueTable.java
@@ -0,0 +1,19 @@
+package sks.utilities.config_reader.annotations.config.database;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation is to be used on the class which will behave as the config database table's/view's value taker in java.
+ * The {@link ConfigKeyValueTable#value tableName} is the config database's table/view name
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ConfigKeyValueTable {
+    /**
+     * The config table/view's name
+     */
+    String value();
+}
